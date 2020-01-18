f8dc0e575e95abc4db2398238789d828e9f84c22
Kimberly Cable <cableredi@comcast.net>
2020-01-18 13:40:43 -0700

    removed tests

--- src/Components/Applications/ApplicationForms/AddApplication.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AddApplication from './AddApplication';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AddApplication />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/Components/Applications/ApplicationForms/UpdateApplication.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import UpdateApplication from './UpdateApplication';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<UpdateApplication />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
