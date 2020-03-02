896a5f91bdb3973888cdc352aa7e0a82e0fe87b6
Kimberly Cable <cableredi@comcast.net>
2020-03-01 20:34:05 -0700

    configuration fix

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-  API_ENDPOINT_APPLICATIONS: `http://localhost:8000/api/applications`,
-  API_ENDPOINT_BUGS: `http://localhost:8000/api/bugs`,
-  API_ENDPOINT_AUTH: `http://localhost:8000/api/auth`,
-  API_ENDPOINT_USERS: `http://localhost:8000/api/users`,
+  API_ENDPOINT_APPLICATIONS: `https://sheltered-atoll-29396.herokuapp.com/api/applications`,
+  API_ENDPOINT_BUGS: `https://sheltered-atoll-29396.herokuapp.com/api/bugs`,
+  API_ENDPOINT_AUTH: `https://sheltered-atoll-29396.herokuapp.com/api/auth`,
+  API_ENDPOINT_USERS: `https://sheltered-atoll-29396.herokuapp.com/api/users`,
   API_KEY: process.env.REACT_APP_API_KEY,
   TOKEN_KEY: 'bedbugs-client-auth-token',
 }
